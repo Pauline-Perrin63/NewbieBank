62b362293646bc72bc7d0600d415d4364c6d2910
Quentin Reymann <quentin.reymann67@gmail.com>
2019-04-26 09:56:06 +0200

    Fix Persistence problem and Mapping updates

--- src/main/java/com/adaming/model/VirementBancaire.java
+++ src/main/java/com/adaming/model/VirementBancaire.java
@@ -10,9 +10,9 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class VirementBancaire implements Serializable {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idVirement;
 	private double montantVirement;
 	private String destinataire;
@@ -23,12 +23,14 @@ public class VirementBancaire implements Serializable {
 	CompteBancaire virementEmis;
 	@ManyToOne
 	CompteBancaire virementRecu;
+
 	public VirementBancaire() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public VirementBancaire(int idVirement, double montantVirement, String destinataire, int idDestinataire,
-			String emetteur, int idEmetteur, CompteBancaire compteDest, CompteBancaire comptEmet) {
+			String emetteur, int idEmetteur, CompteBancaire virementEmis, CompteBancaire virementRecu) {
 		super();
 		this.idVirement = idVirement;
 		this.montantVirement = montantVirement;
@@ -36,62 +38,79 @@ public class VirementBancaire implements Serializable {
 		this.idDestinataire = idDestinataire;
 		this.emetteur = emetteur;
 		this.idEmetteur = idEmetteur;
-		this.compteDest = compteDest;
-		this.comptEmet = comptEmet;
+		this.virementEmis = virementEmis;
+		this.virementRecu = virementRecu;
 	}
+
 	public int getIdVirement() {
 		return idVirement;
 	}
+
 	public void setIdVirement(int idVirement) {
 		this.idVirement = idVirement;
 	}
+
 	public double getMontantVirement() {
 		return montantVirement;
 	}
+
 	public void setMontantVirement(double montantVirement) {
 		this.montantVirement = montantVirement;
 	}
+
 	public String getDestinataire() {
 		return destinataire;
 	}
+
 	public void setDestinataire(String destinataire) {
 		this.destinataire = destinataire;
 	}
+
 	public int getIdDestinataire() {
 		return idDestinataire;
 	}
+
 	public void setIdDestinataire(int idDestinataire) {
 		this.idDestinataire = idDestinataire;
 	}
+
 	public String getEmetteur() {
 		return emetteur;
 	}
+
 	public void setEmetteur(String emetteur) {
 		this.emetteur = emetteur;
 	}
+
 	public int getIdEmetteur() {
 		return idEmetteur;
 	}
+
 	public void setIdEmetteur(int idEmetteur) {
 		this.idEmetteur = idEmetteur;
 	}
-	public CompteBancaire getCompteDest() {
-		return compteDest;
+
+	public CompteBancaire getVirementEmis() {
+		return virementEmis;
 	}
-	public void setCompteDest(CompteBancaire compteDest) {
-		this.compteDest = compteDest;
+
+	public void setVirementEmis(CompteBancaire virementEmis) {
+		this.virementEmis = virementEmis;
 	}
-	public CompteBancaire getComptEmet() {
-		return comptEmet;
+
+	public CompteBancaire getVirementRecu() {
+		return virementRecu;
 	}
-	public void setComptEmet(CompteBancaire comptEmet) {
-		this.comptEmet = comptEmet;
+
+	public void setVirementRecu(CompteBancaire virementRecu) {
+		this.virementRecu = virementRecu;
 	}
+
 	@Override
 	public String toString() {
 		return "VirementBancaire [idVirement=" + idVirement + ", montantVirement=" + montantVirement + ", destinataire="
 				+ destinataire + ", idDestinataire=" + idDestinataire + ", emetteur=" + emetteur + ", idEmetteur="
-				+ idEmetteur + ", compteDest=" + compteDest + ", comptEmet=" + comptEmet + "]";
+				+ idEmetteur + ", virementEmis=" + virementEmis + ", virementRecu=" + virementRecu + "]";
 	}
-	
+
 }
