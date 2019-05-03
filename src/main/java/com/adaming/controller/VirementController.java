30bfdb22d88d91afdfcc1f4556ede7cfab1efb40
Quentin Reymann <quentin.reymann67@gmail.com>
2019-05-03 12:26:03 +0200

    modification of controllers

--- src/main/java/com/adaming/controller/VirementController.java
+++ src/main/java/com/adaming/controller/VirementController.java
@@ -3,13 +3,15 @@ package com.adaming.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adaming.model.VirementBancaire;
@@ -29,35 +31,41 @@ public class VirementController {
 	public VirementController(VirementBancaireServiceImpl virementServiceImpl) {
 		this.virementService = virementServiceImpl;
 	}
+	
+	@PostMapping(value = "/ajouterVirement", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public VirementBancaire saveVirement(@RequestBody VirementBancaire virementBancaire) {
+		return this.virementService.saveVirement(virementBancaire);
+	}
 
-	@RequestMapping(value = "/ajouterVirement", method = RequestMethod.POST)
-	public ResponseEntity<VirementBancaire> createVirement(@RequestBody VirementBancaire virement) {
-		virement = this.virementService.saveVirement(virement);
-		return new ResponseEntity<VirementBancaire>(virement, HttpStatus.OK);
+	@PutMapping("/{id}")
+	public VirementBancaire updateVirement (@RequestBody VirementBancaire virementBancaire, @PathVariable int id) {
+		if(virementService.updateVirement(virementBancaire)== null) {
+			return null;
+		} else {
+			return virementService.updateVirement(virementBancaire);
+		}
 	}
-	
-	@RequestMapping(value = "/modifierVirement", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<VirementBancaire> updateVirement(@RequestBody VirementBancaire Virement) {
-		Virement = this.virementService.updateVirement(Virement);
-		return new ResponseEntity<VirementBancaire>(Virement, HttpStatus.OK);
+
+	@DeleteMapping("/{id}")
+	public void deleteVirement(@PathVariable int id) {
+		if(virementService.findByIdVirement(id)!= null) {
+			virementService.deleteVirement(virementService.findByIdVirement(id));
+		}
+	} 
+
+	@GetMapping(value = "/findById/{id}")
+	public VirementBancaire getVirementById(@PathVariable int id) {
+		if(virementService.findByIdVirement(id)== null) {
+			return null;
+		} else {
+			return virementService.findByIdVirement(id);
+		}
 	}
-	
-	@RequestMapping(value = "/supprimerVirement", method = RequestMethod.DELETE)
-	public ResponseEntity<VirementBancaire> deleteVirement(@RequestBody VirementBancaire virement) {
-		this.virementService.deleteVirement(virement);
-		return new ResponseEntity<VirementBancaire>(virement, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/getVirementById", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<VirementBancaire> getVirementById(@RequestBody Integer id) {
-		VirementBancaire Virement = this.virementService.findByIdVirement(id);
-		return new ResponseEntity<VirementBancaire>(Virement, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/Virements", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<VirementBancaire>> getAllVirements() {
-		List<VirementBancaire> Virements = this.virementService.findAllVirement();
-		return new ResponseEntity<List<VirementBancaire>>(Virements, HttpStatus.OK);
+
+	@GetMapping(value = "/all")
+	public List<VirementBancaire> getAllVirement() {
+		List<VirementBancaire> vb = virementService.findAllVirement();
+	return vb;
 	}
 
 }
